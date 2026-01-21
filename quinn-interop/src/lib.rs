9aa0f5fd75fb3d196afb949196a7220c327668b6
Johannes Frisch <johannes.frisch@stud.tu-darmstadt.de>
2026-01-21 19:03:43 +0100

    change transport config to default quinn

--- quinn-interop/src/lib.rs
+++ quinn-interop/src/lib.rs
@@ -1,20 +1,9 @@
-use std::{convert::TryInto, sync::Arc, time::Duration};
+use std::sync::Arc;
 
 use quinn::TransportConfig;
 
 pub fn transport_config() -> Arc<TransportConfig> {
-    let mut transport_config = TransportConfig::default();
-    transport_config
-        // quic-go's default, required for loss/corruption handling tests to pass reliably
-        .max_idle_timeout(Some(Duration::from_secs(30).try_into().unwrap()))
-        .initial_rtt(Duration::from_millis(100))
-        // https://github.com/quic-interop/quic-interop-runner/issues/397
-        .enable_segmentation_offload(false)
-        // Don't bother probing a known network environment, and avoid
-        // https://github.com/quic-interop/quic-interop-runner/issues/398
-        .mtu_discovery_config(None)
-        // Known interface MTU, minus conservative IPv6 and UDP header sizes
-        .initial_mtu(1200);
+    let transport_config = TransportConfig::default();
     Arc::new(transport_config)
 }
 
